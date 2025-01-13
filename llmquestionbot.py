08fcabd0a2e885f0c0319ef3c6d61dcdeb7278ff
mwhmusic330 <m.w.haburay.music@gmail.com>
2025-01-13 16:19:17 +0000

    abstracted functions. I'll get rid of the hard coded file path but I just wanted to get it working first.

---
+++ llmquestionbot.py
@@ -0,0 +1,58 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline, HuggingFaceEndpoint
+import getpass, os
+from langchain_core.messages import (
+    HumanMessage,
+    SystemMessage,
+)
+
+path="/home/michael/hugginfacestuff/token2"
+
+def key(path):
+    with open(path, 'r') as f: 
+        apikey = f.read().strip()
+    if not apikey:
+        with open(path, 'w') as f:
+            apikey = getpass.getpass(
+                "Enter your Hugging Face API key: "
+            )
+            f.write(apikey)
+           
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = apikey
+
+
+
+
+def llm_params():
+    llm = HuggingFaceEndpoint(
+        repo_id="HuggingFaceH4/zephyr-7b-beta",
+        task="text-generation",
+        max_new_tokens=10000,
+        do_sample=False,
+        repetition_penalty=1.03,
+    )
+
+    chat_model = ChatHuggingFace(llm=llm)
+    
+    return chat_model
+
+def ai_messages(chat_model):
+    messages = [
+        SystemMessage(content="You're a helpful assistant"),
+        HumanMessage(
+            content=input("Hey man, whatcha need?\n")
+        ),
+    ]
+
+    ai_msg = chat_model.invoke(messages)
+
+    print(ai_msg.content)
+
+
+
+def main():
+    keyfunc = key(path)
+    modelparams = llm_params()
+    messageparams = ai_messages(modelparams)
+
+if __name__ == '__main__':
+    main()
